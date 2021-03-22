f9334a286cf7ee0730058bab3ad9efbf8cd5e936
ianic1999 <uiie2639@scs.ubbcluj.ro>
2021-03-22 11:21:56 +0200

    implement 2 tests

---
+++ ssvv-project/src/test/java/UI/uiTest.java
@@ -0,0 +1,42 @@
+package UI;
+
+import Domain.Student;
+import Exceptions.ValidatorException;
+import Repository.XMLFileRepository.NotaXMLRepo;
+import Repository.XMLFileRepository.StudentXMLRepo;
+import Repository.XMLFileRepository.TemaLabXMLRepo;
+import Service.XMLFileService.NotaXMLService;
+import Service.XMLFileService.StudentXMLService;
+import Service.XMLFileService.TemaLabXMLService;
+import Validator.NotaValidator;
+import Validator.StudentValidator;
+import Validator.TemaLabValidator;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class uiTest {
+
+    StudentValidator vs=new StudentValidator();
+    TemaLabValidator vt=new TemaLabValidator();
+    NotaValidator vn=new NotaValidator();
+    StudentXMLRepo strepo=new StudentXMLRepo(vs,"StudentiXML.xml");
+    TemaLabXMLRepo tmrepo=new TemaLabXMLRepo(vt,"TemaLaboratorXML.xml");
+    NotaXMLRepo ntrepo=new NotaXMLRepo(vn,"NotaXML.xml");
+    StudentXMLService stsrv=new StudentXMLService(strepo);
+    TemaLabXMLService tmsrv=new TemaLabXMLService(tmrepo);
+    NotaXMLService ntsrv=new NotaXMLService(ntrepo);
+    ui ui=new ui(stsrv,tmsrv,ntsrv);
+
+    @Test
+    public void tc_1_addStudent_success() throws ValidatorException {
+        String[] params = {"111", "Student", "937", "student@gmail.com", "professor"};
+        stsrv.add(params);
+    }
+
+    @Test(expected = ValidatorException.class)
+    public void tc_1_addStudent_invalid_group() throws ValidatorException {
+        String[] params = {"112", "Student", "0", "student@gmail.com", "professor"};
+        stsrv.add(params);
+    }
+}
