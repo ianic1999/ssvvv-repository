ce3cccebc2666345c8c7439fcea09ef6644900c1
Razvan <razvantoma1573@gmail.com>
2021-03-22 12:56:47 +0200

    Update StudentValidator.java

--- src/main/java/Validator/StudentValidator.java
+++ src/main/java/Validator/StudentValidator.java
@@ -7,11 +7,11 @@ public class StudentValidator implements IValidator<Student> {
 
     public void validate(Student s) throws ValidatorException {
         String errors="";
-        if(s.getId().equals("")){
+        if(s.getId() == null || s.getId().equals("")){
             //throw new ValidatorException("Id invalid\n");
             errors+="Id invalid\n";
         }
-        if(s.getNume().equals("") || s.getNume()==null){
+        if(s.getNume()==null || s.getNume().equals("")){
             //throw new ValidatorException("Nume invalid\n");
             errors+="Nume invalid\n";
         }
@@ -19,9 +19,12 @@ public class StudentValidator implements IValidator<Student> {
             //throw new ValidatorException("Grupa invalida\n");
             errors+="Grupa invalid\n";
         }
-        if(s.getEmail().equals("") || s.getEmail()==null){
+        if( s.getEmail()==null || s.getEmail().equals("") || !s.getEmail().matches("\\b[\\w.%-]+@[-.\\w]+\\.[A-Za-z]{2,4}\\b")){
             //throw new ValidatorException("Email invalid\n");
             errors+="Email invalid\n";
+        }
+        if( s.getIndrumator()==null || s.getIndrumator().equals("")) {
+            errors+="Indrumator invalid\n";
         }
         if (errors.length()!=0){
             throw  new ValidatorException(errors);
